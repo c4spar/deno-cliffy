69004629e8b49e7f7b8102ed322dd8073ddfb2f0
Benjamin Fischer <c4spar@gmx.de>
2020-03-22 23:28:07 +0100

    refactor(flags): refactor `string` type

--- packages/flags/lib/types/string.ts
+++ packages/flags/lib/types/string.ts
@@ -2,7 +2,5 @@ import { IFlagArgument, IFlagOptions, ITypeHandler } from '../types.ts';
 
 export const string: ITypeHandler<string> = ( option: IFlagOptions, arg: IFlagArgument, value: string | false ): string | undefined => {
 
-    if ( typeof value === 'string' && value.length ) {
-        return value;
-    }
+    return value || undefined;
 };
