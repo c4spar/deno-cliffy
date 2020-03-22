5feeb77ef7a7ae45b2e2abd9c332ca845b5dc4af
Benjamin Fischer <c4spar@gmx.de>
2020-03-22 23:41:03 +0100

    refactor(command): update description of `completions` command

--- packages/command/commands/completions.ts
+++ packages/command/commands/completions.ts
@@ -1,3 +1,4 @@
+import { bold, dim, italic } from 'https://deno.land/std/fmt/colors.ts';
 import { BaseCommand } from '../lib/base-command.ts';
 import { DefaultCommand } from '../lib/default-command.ts';
 import { BashCompletionsCommand } from './completions/bash.ts';
@@ -12,7 +13,20 @@ export class CompletionsCommand extends DefaultCommand {
 
         super();
 
-        this.description( 'Generate shell completions for zsh and bash.' )
+        this.description( `Generate shell completions for zsh and bash.
+
+${ dim( bold( 'Bash completions:' ) ) }
+
+To enable bash completions for this program add following line to your ${ dim( italic( '~/.bashrc' ) ) }.
+
+    ${ dim( italic( 'source <(command-name completions bash)' ) ) }
+
+${ dim( bold( 'Zsh completions:' ) ) }
+
+To enable zsh completions for this program add following line to your ${ dim( italic( '~/.zshrc' ) ) }.
+
+    ${ dim( italic( 'source <(command-name completions zsh)' ) ) }
+` )
             .command( 'zsh', new ZshCompletionsCommand( this.parent ) )
             .command( 'bash', new BashCompletionsCommand( this.parent ) )
             .reset();
