a3c5c72edc652e319b6403afcadbdb00cd5211e7
Benjamin Fischer <c4spar@gmx.de>
2020-03-31 00:02:46 +0200

    refactor(command): update `completions` description

--- packages/command/commands/completions.ts
+++ packages/command/commands/completions.ts
@@ -17,16 +17,27 @@ export class CompletionsCommand extends DefaultCommand {
 
 ${ dim( bold( 'Bash completions:' ) ) }
 
-To enable bash completions for this program add following line to your ${ dim( italic( '~/.bashrc' ) ) }.
+To enable bash completions for this program add following line to your ${ dim( italic( '~/.bashrc' ) ) }:
 
     ${ dim( italic( 'source <(command-name completions bash)' ) ) }
 
+or create a separate file in the ${ dim( italic( 'bash_completion.d' ) ) } directory:
+
+    ${ dim( italic( `${ parent.getPath() } completions bash > /usr/local/etc/bash_completion.d/${ parent.getPath() }.bash` ) ) }
+    ${ dim( italic( `source /usr/local/etc/bash_completion.d/${ parent.getPath() }.bash` ) ) }
+
 ${ dim( bold( 'Zsh completions:' ) ) }
 
-To enable zsh completions for this program add following line to your ${ dim( italic( '~/.zshrc' ) ) }.
+To enable zsh completions for this program add following line to your ${ dim( italic( '~/.zshrc' ) ) }:
 
     ${ dim( italic( 'source <(command-name completions zsh)' ) ) }
+
+or create a separate file in the ${ dim( italic( 'zsh_completion.d' ) ) } directory:
+
+    ${ dim( italic( `${ parent.getPath() } completions zsh > /usr/local/etc/zsh_completion.d/${ parent.getPath() }.zsh` ) ) }
+    ${ dim( italic( `source /usr/local/etc/zsh_completion.d/${ parent.getPath() }.zsh` ) ) }
 ` )
+
             .default( 'help' )
             .command( 'zsh', new ZshCompletionsCommand( this.parent ) )
             .command( 'bash', new BashCompletionsCommand( this.parent ) )
