2b9608c0fddeebe197054cf3b0656b8dc8a9448f
Benjamin Fischer <c4spar@gmx.de>
2020-03-30 23:39:56 +0200

    feat(command): add `command` type

---
+++ packages/command/types/command-list.ts
@@ -0,0 +1,14 @@
+import { BaseCommand } from '../lib/base-command.ts';
+import { StringType } from './string.ts';
+
+export class CommandListType extends StringType {
+
+    constructor( protected cmd: BaseCommand ) {
+        super();
+    }
+
+    public complete(): string[] {
+
+        return this.cmd.getCommands().map( ( cmd => cmd.getName() ) );
+    }
+}
