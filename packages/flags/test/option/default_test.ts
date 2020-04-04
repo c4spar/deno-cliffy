2fbfd54d3a89c8f657bc81babefc74d8cf10e24b
Benjamin Fischer <c4spar@gmx.de>
2020-04-04 18:43:57 +0200

    test(flags): add default value test

---
+++ packages/flags/test/option/default_test.ts
@@ -0,0 +1,58 @@
+import { parseFlags } from '../../lib/flags.ts';
+import { IParseOptions, OptionType } from '../../lib/types.ts';
+import { assertEquals } from '../lib/assert.ts';
+
+const options = <IParseOptions>{
+    allowEmpty: true,
+    flags: [ {
+        name: 'boolean',
+        aliases: [ 'b' ],
+        type: OptionType.BOOLEAN,
+        default: false
+    }, {
+        name: 'string',
+        aliases: [ 's' ],
+        type: OptionType.STRING,
+        default: '0'
+    }, {
+        name: 'number',
+        aliases: [ 'n' ],
+        type: OptionType.NUMBER,
+        default: 0
+    }, {
+        name: 'boolean2',
+        aliases: [ 'B' ],
+        type: OptionType.BOOLEAN,
+        default: true
+    }, {
+        name: 'string2',
+        aliases: [ 'S' ],
+        type: OptionType.STRING,
+        default: '1'
+    }, {
+        name: 'number2',
+        aliases: [ 'N' ],
+        type: OptionType.NUMBER,
+        default: 1
+    } ]
+};
+
+Deno.test( function flags_optionDefault_defaultValues() {
+
+    const { flags, unknown, literal } = parseFlags( [], options );
+
+    assertEquals( flags, { boolean: false, string: '0', number: 0, boolean2: true, string2: '1', number2: 1 } );
+    assertEquals( unknown, [] );
+    assertEquals( literal, [] );
+} );
+
+Deno.test( function flags_optionDefault_defaultValues() {
+
+    const { flags, unknown, literal } = parseFlags( [ '-b', '1', '-s', '1', '-n', '1', '-B', '0', '-S', '0', '-N', '0' ], options );
+
+    assertEquals( flags, { boolean: true, string: '1', number: 1, boolean2: false, string2: '0', number2: 0 } );
+    assertEquals( unknown, [] );
+    assertEquals( literal, [] );
+} );
+
+await Deno.runTests();
