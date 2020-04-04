6e3bc57240bb4aa37c08b2a6f35684581f7aa2d8
Benjamin Fischer <c4spar@gmx.de>
2020-04-04 19:46:57 +0200

    test(flags): add value test

---
+++ packages/flags/test/option/value_test.ts
@@ -0,0 +1,29 @@
+import { parseFlags } from '../../lib/flags.ts';
+import { IParseOptions } from '../../lib/types.ts';
+import { assertEquals } from '../lib/assert.ts';
+
+const options = <IParseOptions>{
+    stopEarly: false,
+    allowEmpty: false,
+    flags: [ {
+        name: 'flag',
+        aliases: [ 'f' ],
+        value( value: string ): string[] {
+            return [ value ];
+        }
+    }, {
+        name: 'flag2',
+        aliases: [ 'F' ]
+    } ]
+};
+
+Deno.test( function flags_optionVariadic_optional() {
+
+    const { flags, unknown, literal } = parseFlags( [ '-f', '1', '-F', '1' ], options );
+
+    assertEquals( flags, { flag: [ '1' ], flag2: '1' } );
+    assertEquals( unknown, [] );
+    assertEquals( literal, [] );
+} );
+
+await Deno.runTests();
