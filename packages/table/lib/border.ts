866b71ec781d0cbf6f91667f4ba45f76dca46c0d
Benjamin Fischer <c4spar@gmx.de>
2020-07-09 23:02:49 +0200

    feat(table): make border chars customizable

--- packages/table/lib/border.ts
+++ packages/table/lib/border.ts
@@ -1,4 +1,22 @@
-export const border = {
+export interface IBorder {
+    top: string;
+    topMid: string;
+    topLeft: string;
+    topRight: string;
+    bottom: string;
+    bottomMid: string;
+    bottomLeft: string;
+    bottomRight: string;
+    left: string;
+    leftMid: string;
+    mid: string;
+    midMid: string;
+    right: string;
+    rightMid: string;
+    middle: string;
+}
+
+export const border: IBorder = {
     top: '─',
     topMid: '┬',
     topLeft: '┌',
