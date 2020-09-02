495758fcc277fccb6e4131d68d2712d86031bf0c
Benjamin Fischer <c4spar@gmx.de>
2020-09-02 19:54:37 +0200

    chore(upgrade): deno/std v0.67.0

--- examples/command/examples.ts
+++ examples/command/examples.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S deno run
 
-import { red } from "https://deno.land/std@0.66.0/fmt/colors.ts";
+import { red } from "https://deno.land/std@0.67.0/fmt/colors.ts";
 import { Command } from "../../command/command.ts";
 
 await new Command()
