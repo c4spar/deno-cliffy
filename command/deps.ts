43204a53e0711ce371372b1433f426ad02e61e96
Benjamin Fischer <c4spar@gmx.de>
2020-09-02 19:54:37 +0200

    chore(upgrade): deno/std v0.67.0

--- command/deps.ts
+++ command/deps.ts
@@ -7,8 +7,8 @@ export {
   italic,
   blue,
   bold,
-} from "https://deno.land/std@0.66.0/fmt/colors.ts";
+} from "https://deno.land/std@0.67.0/fmt/colors.ts";
 
 export {
   existsSync,
-} from "https://deno.land/std@0.66.0/fs/exists.ts";
+} from "https://deno.land/std@0.67.0/fs/exists.ts";
--- table/deps.ts
+++ table/deps.ts
@@ -1,3 +1,3 @@
 export {
   stripColor,
-} from "https://deno.land/std@0.66.0/fmt/colors.ts";
+} from "https://deno.land/std@0.67.0/fmt/colors.ts";
