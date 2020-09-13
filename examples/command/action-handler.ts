a98e8ff1cc2960f0319e630f9e35eb019e1aa88f
Benjamin Fischer <c4spar@gmx.de>
2020-09-13 17:15:47 +0200

    chore(lint): fix lint error

--- examples/command/action-handler.ts
+++ examples/command/action-handler.ts
@@ -5,7 +5,7 @@ import { Command } from "../../command/mod.ts";
 await new Command()
   .command("rm <dir>", "Remove directory.")
   .option("-r, --recursive [recursive:boolean]", "Remove recursively")
-  // deno-lint-ignore no-explicit-any
+  // deno-lint-ignore no-explicit-any no-undef
   .action(({ recursive }: any, dir: string) => {
     console.log("remove " + dir + (recursive ? " recursively" : ""));
   })
