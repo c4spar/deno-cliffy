5dbe3531e06ba7d76950149c3eb5b47a4f1f1876
Benjamin Fischer <c4spar@gmx.de>
2020-09-20 16:18:43 +0200

    chore(upgrade): deno/std v0.70.0

--- dev_deps.ts
+++ dev_deps.ts
@@ -4,13 +4,13 @@ export {
   assertStrictEquals,
   assertThrows,
   assertThrowsAsync,
-} from "https://deno.land/std@0.69.0/testing/asserts.ts";
+} from "https://deno.land/std@0.70.0/testing/asserts.ts";
 
 export {
   bold,
   red,
   stripColor,
-} from "https://deno.land/std@0.69.0/fmt/colors.ts";
+} from "https://deno.land/std@0.70.0/fmt/colors.ts";
 
 export {
   lt,
--- prompt/deps.ts
+++ prompt/deps.ts
@@ -7,4 +7,4 @@ export {
   red,
   underline,
   stripColor,
-} from "https://deno.land/std@0.69.0/fmt/colors.ts";
+} from "https://deno.land/std@0.70.0/fmt/colors.ts";
