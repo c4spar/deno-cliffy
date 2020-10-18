e99969022fad6d7ab42ed9df75c81421de95893b
Benjamin Fischer <c4spar@gmx.de>
2020-10-18 14:05:34 +0200

    fix formatting

--- flags/test/option/unknown_test.ts
+++ flags/test/option/unknown_test.ts
@@ -2,7 +2,7 @@ import { assertEquals } from "../../../dev_deps.ts";
 import { parseFlags } from "../../flags.ts";
 import type { IParseOptions } from "../../types.ts";
 
-const options = <IParseOptions>{
+const options = <IParseOptions> {
   flags: [{
     name: "flag",
     aliases: ["f"],
@@ -10,7 +10,10 @@ const options = <IParseOptions>{
 };
 
 Deno.test("unknown flags", () => {
-  const { flags, unknown, literal } = parseFlags(["-f", "foo", "-", "--", "bar"], options);
+  const { flags, unknown, literal } = parseFlags(
+    ["-f", "foo", "-", "--", "bar"],
+    options,
+  );
 
   assertEquals(flags, { flag: true });
   assertEquals(unknown, ["foo", "-"]);
