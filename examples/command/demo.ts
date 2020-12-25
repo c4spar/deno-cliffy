87223aadf568f45e510863ff13920f68e085d6b7
Benjamin Fischer <c4spar@gmx.de>
2020-12-25 21:33:03 +0100

    docs(command): update examples

---
+++ examples/command/demo.ts
@@ -0,0 +1,28 @@
+#!/usr/bin/env -S deno run
+
+import { Command } from "../../command/command.ts";
+import { CompletionsCommand } from "../../command/completions/mod.ts";
+import { HelpCommand } from "../../command/help/mod.ts";
+
+await new Command()
+  .name("cliffy")
+  .version("1.0.0")
+  .description(`Command line framework for Deno`)
+  .option("-p, --beep", "Some option.", {
+    default: true,
+  })
+  .option("--no-beep", "Negate beep.")
+  .option("-f, --foo [value:string]", "Some string value.", {
+    default: "foo",
+  })
+  .option("-b, --bar [value:number]", "Some numeric value.", {
+    default: 89,
+    depends: ["foo"],
+  })
+  .option("-B, --baz <value:boolean>", "Some boolean value.", {
+    default: true,
+    conflicts: ["beep"],
+  })
+  .command("help", new HelpCommand().global())
+  .command("completions", new CompletionsCommand())
+  .parse(Deno.args);
