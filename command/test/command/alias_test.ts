45fc6784140b6c419e8e9e9c96b1fe9bdadd15f8
Benjamin Fischer <61995275+c4spar@users.noreply.github.com>
2021-03-18 20:07:14 +0100

    fix(command): command alias not working (#167)

---
+++ command/test/command/alias_test.ts
@@ -0,0 +1,56 @@
+import { assertEquals, assertThrowsAsync } from "../../../dev_deps.ts";
+import { Command } from "../../command.ts";
+
+Deno.test("command - alias - command with alias 1", async () => {
+  const cmd = new Command()
+    .throwErrors()
+    .command("foo, bar")
+    .option("-b, --baz", "...");
+
+  const { options: fooOptions } = await cmd.parse(["foo", "-b"]);
+  const { options: barOptions } = await cmd.parse(["bar", "-b"]);
+
+  assertEquals(fooOptions, { baz: true });
+  assertEquals(barOptions, { baz: true });
+});
+
+Deno.test("command - alias - command with alias 2", async () => {
+  const cmd = new Command()
+    .throwErrors()
+    .command("foo")
+    .alias("bar")
+    .option("-b, --baz", "...");
+
+  const { options: fooOptions } = await cmd.parse(["foo", "-b"]);
+  const { options: barOptions } = await cmd.parse(["bar", "-b"]);
+
+  assertEquals(fooOptions, { baz: true });
+  assertEquals(barOptions, { baz: true });
+});
+
+Deno.test("command - alias - duplicate command alias name 1", async () => {
+  await assertThrowsAsync(
+    async () => {
+      await new Command()
+        .throwErrors()
+        .command("foo, foo")
+        .parse([]);
+    },
+    Error,
+    `Duplicate command alias "foo".`,
+  );
+});
+
+Deno.test("command - alias - duplicate command alias name 2", async () => {
+  await assertThrowsAsync(
+    async () => {
+      await new Command()
+        .throwErrors()
+        .command("foo")
+        .alias("foo")
+        .parse([]);
+    },
+    Error,
+    `Duplicate command alias "foo".`,
+  );
+});
