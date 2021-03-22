37b07e9aeadaa292ae12c92c8365b57b5fbba00d
Benjamin Fischer <61995275+c4spar@users.noreply.github.com>
2021-03-23 00:27:39 +0100

    test(command): add integration tests (#169)

---
+++ command/test/integration/command.ts
@@ -0,0 +1,25 @@
+import { Command, CompletionsCommand, HelpCommand } from "../../mod.ts";
+
+const cmd = new Command()
+  .version("1.0.0")
+  .name("completions-test")
+  .description("Completions test.")
+  .globalOption("-g, --global <val:boolean>", "Foo option.")
+  .option("-m, --main <val:boolean>", "Bar option.")
+  .default("help")
+  // foo
+  .command(
+    "foo",
+    new Command()
+      .description("Foo command.")
+      .option("-f, --foo", "Foo option.")
+      // bar
+      .command("bar", "Bar command.")
+      .option("-b, --bar", "Bar option."),
+  )
+  .command("help", new HelpCommand().global())
+  .command("completions", new CompletionsCommand());
+
+if (import.meta.main) {
+  await cmd.parse();
+}
---
+++ command/test/integration/test.ts
@@ -0,0 +1,41 @@
+import { assertEquals, dirname, expandGlob } from "../../../dev_deps.ts";
+
+const baseDir = `${dirname(import.meta.url).replace("file://", "")}`;
+
+for await (const file of expandGlob(`${baseDir}/fixtures/*.in`)) {
+  if (file.isFile) {
+    const name = file.name.replace(/_/g, " ").replace(".in", "");
+    const outPath = file.path.replace(/\.in$/, ".out");
+    Deno.test({
+      name: `command - integration - ${name}`,
+      ignore: Deno.build.os === "windows",
+      async fn() {
+        const [cmd, expected] = await Promise.all([
+          Deno.readTextFile(file.path),
+          Deno.readTextFile(outPath),
+        ]);
+        const output: string = await runCommand(cmd.split(" "));
+        assertEquals(output, expected);
+      },
+    });
+  }
+}
+
+async function runCommand(cmd: Array<string>): Promise<string> {
+  const process = Deno.run({
+    stdout: "piped",
+    cmd: [
+      "deno",
+      "run",
+      "--unstable",
+      "--allow-all",
+      `${baseDir}/command.ts`,
+      ...cmd,
+    ],
+  });
+
+  const output = await process.output();
+  process.close();
+
+  return new TextDecoder().decode(output);
+}
