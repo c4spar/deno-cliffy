3690d25d10a0f3565e547498e27ace79de7010af
Benjamin Fischer <c4spar@gmx.de>
2021-05-16 00:10:36 +0200

    allow numbers in enum type

--- command/types/enum.ts
+++ command/types/enum.ts
@@ -3,19 +3,22 @@ import type { ITypeInfo } from "../types.ts";
 import { InvalidTypeError } from "../../flags/_errors.ts";
 
 /** Enum type. Allows only provided values. */
-export class EnumType<T extends string> extends Type<T> {
-  constructor(
-    private readonly values: ReadonlyArray<T>,
-  ) {
+export class EnumType<T extends string | number> extends Type<T> {
+  private readonly allowedValues: ReadonlyArray<T>;
+
+  constructor(values: ReadonlyArray<T>) {
     super();
+    this.allowedValues = values;
   }
 
   public parse(type: ITypeInfo): T {
-    if (!this.values.includes(type.value as T)) {
-      throw new InvalidTypeError(type, this.values.slice());
+    for (const value of this.allowedValues) {
+      if (value.toString() === type.value) {
+        return value;
+      }
     }
 
-    return type.value as T;
+    throw new InvalidTypeError(type, this.allowedValues.slice());
   }
 
   public complete(): string[] {
