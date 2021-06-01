0f412055c70ccfdad719588ae7f614e9530766bb
Benjamin Fischer <c4spar@gmx.de>
2021-06-01 18:47:24 +0200

    chore(upgrade): upgrade dev deps

--- dev_deps.ts
+++ dev_deps.ts
@@ -14,4 +14,4 @@ export { dirname } from "https://deno.land/std@0.97.0/path/mod.ts";
 export { expandGlob } from "https://deno.land/std@0.97.0/fs/mod.ts";
 export type { WalkEntry } from "https://deno.land/std@0.97.0/fs/mod.ts";
 
-export { gt, lt } from "https://deno.land/x/semver@v1.3.0/mod.ts";
+export { gt, lt } from "https://deno.land/x/semver@v1.4.0/mod.ts";
