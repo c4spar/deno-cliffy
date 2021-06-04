166179e0a6f304e41875f212d0a330f38d8fd1e0
Benjamin Fischer <c4spar@gmx.de>
2021-06-04 18:09:09 +0200

    update tests

--- table/test/align_test.ts
+++ table/test/align_test.ts
@@ -51,3 +51,36 @@ Deno.test("table - align - align row and cell", () => {
 └─────────────┴─────────────┴─────────────┘`.slice(1),
   );
 });
+
+Deno.test("table - align - default direction", () => {
+  const cell = new Cell("foo");
+  const row = new Row(cell);
+  const table = new Table(row);
+  assertEquals(cell.getAlign(), "left");
+  assertEquals(row.getAlign(), "left");
+  assertEquals(table.getAlign(), "left");
+  assertEquals(table[0][0].getAlign(), "left");
+  assertEquals(table[0].getAlign(), "left");
+});
+
+Deno.test("table - align - override direction", () => {
+  const cell = new Cell("foo").align("center");
+  const row = new Row(cell).align("center");
+  const table = new Table(row).align("center");
+  assertEquals(cell.getAlign(), "center");
+  assertEquals(row.getAlign(), "center");
+  assertEquals(table.getAlign(), "center");
+  assertEquals(table[0][0].getAlign(), "center");
+  assertEquals(table[0].getAlign(), "center");
+});
+
+Deno.test("table - align - inherit direction", () => {
+  const cell = new Cell("foo");
+  const row = new Row(cell);
+  const table = new Table(row).align("right");
+  assertEquals(cell.getAlign(), "left");
+  assertEquals(row.getAlign(), "left");
+  assertEquals(table.getAlign(), "right");
+  assertEquals(table[0][0].getAlign(), "left");
+  assertEquals(table[0].getAlign(), "left");
+});
