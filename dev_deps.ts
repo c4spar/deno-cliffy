534094b63dc6616a2383fd4b36d1a1d744c1ad49
Benjamin Fischer <61995275+c4spar@users.noreply.github.com>
2021-07-16 13:02:24 +0200

    chore(upgrade): deno/std v0.101.0 (#244)

--- dev_deps.ts
+++ dev_deps.ts
@@ -4,14 +4,14 @@ export {
   assertStrictEquals,
   assertThrows,
   assertThrowsAsync,
-} from "https://deno.land/std@0.99.0/testing/asserts.ts";
+} from "https://deno.land/std@0.101.0/testing/asserts.ts";
 export {
   bold,
   red,
   stripColor,
-} from "https://deno.land/std@0.99.0/fmt/colors.ts";
-export { dirname } from "https://deno.land/std@0.99.0/path/mod.ts";
-export { expandGlob } from "https://deno.land/std@0.99.0/fs/mod.ts";
-export type { WalkEntry } from "https://deno.land/std@0.99.0/fs/mod.ts";
+} from "https://deno.land/std@0.101.0/fmt/colors.ts";
+export { dirname } from "https://deno.land/std@0.101.0/path/mod.ts";
+export { expandGlob } from "https://deno.land/std@0.101.0/fs/mod.ts";
+export type { WalkEntry } from "https://deno.land/std@0.101.0/fs/mod.ts";
 
 export { gt, lt } from "https://deno.land/x/semver@v1.4.0/mod.ts";
