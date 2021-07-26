ad65b8ea6ac66f7ac7f6211e5a98361461418526
Benjamin Fischer <61995275+c4spar@users.noreply.github.com>
2021-07-27 00:48:57 +0200

    chore(upgrade): deno/std v0.103.0 (#254)

--- examples/ansi/demo.ts
+++ examples/ansi/demo.ts
@@ -1,4 +1,4 @@
-import * as stdColors from "https://deno.land/std@0.101.0/fmt/colors.ts";
+import * as stdColors from "https://deno.land/std@0.103.0/fmt/colors.ts";
 import * as ansiEscapes from "../../ansi/ansi_escapes.ts";
 
 const ansiEscapeNames1: Array<keyof typeof ansiEscapes> = [
