4441617353b67c0a25bcfb70573d3403d651d522
Benjamin Fischer <c4spar@gmx.de>
2022-04-06 20:14:32 +0200

    disable options for now and add export

--- command/types/file.ts
+++ command/types/file.ts
@@ -1,14 +1,14 @@
 import { StringType } from "./string.ts";
 
-export interface FileTypeOptions {
-  dirsOnly?: boolean;
-  pattern?: string;
-  ignore?: Array<string>;
-}
+// export interface FileTypeOptions {
+//   dirsOnly?: boolean;
+//   pattern?: string;
+//   ignore?: Array<string>;
+// }
 
 /** Integer type. */
 export class FileType extends StringType {
-  constructor(private readonly opts: FileTypeOptions = {}) {
+  constructor() {
     super();
   }
 
