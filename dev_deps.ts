8c102dfca5ddc11d03ad0438e110a5c4a74c73e5
Benjamin Fischer <c4spar@gmx.de>
2022-04-26 20:12:22 +0200

    chore(upgrade): deno/std@0.136.0, sinon@v13.0.2

--- dev_deps.ts
+++ dev_deps.ts
@@ -5,17 +5,17 @@ export {
   assertRejects,
   assertStrictEquals,
   assertThrows,
-} from "https://deno.land/std@0.134.0/testing/asserts.ts";
+} from "https://deno.land/std@0.136.0/testing/asserts.ts";
 export {
   bold,
   red,
   stripColor,
-} from "https://deno.land/std@0.134.0/fmt/colors.ts";
-export { dirname } from "https://deno.land/std@0.134.0/path/mod.ts";
-export { expandGlob } from "https://deno.land/std@0.134.0/fs/mod.ts";
-export type { WalkEntry } from "https://deno.land/std@0.134.0/fs/mod.ts";
-export { copy } from "https://deno.land/std@0.134.0/streams/conversion.ts";
+} from "https://deno.land/std@0.136.0/fmt/colors.ts";
+export { dirname } from "https://deno.land/std@0.136.0/path/mod.ts";
+export { expandGlob } from "https://deno.land/std@0.136.0/fs/mod.ts";
+export type { WalkEntry } from "https://deno.land/std@0.136.0/fs/mod.ts";
+export { copy } from "https://deno.land/std@0.136.0/streams/conversion.ts";
 
 /* 3rd party */
 export { gt, lt } from "https://deno.land/x/semver@v1.4.0/mod.ts";
-export { default as sinon } from "https://cdn.skypack.dev/sinon@v13.0.1?dts";
+export { default as sinon } from "https://cdn.skypack.dev/sinon@v13.0.2?dts";
--- examples/command/examples.ts
+++ examples/command/examples.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S deno run
 
-import { red } from "https://deno.land/std@0.134.0/fmt/colors.ts";
+import { red } from "https://deno.land/std@0.136.0/fmt/colors.ts";
 import { Command } from "../../command/command.ts";
 
 await new Command()
