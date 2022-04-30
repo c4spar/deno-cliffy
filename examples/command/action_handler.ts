48bcf1ac279a2ac87148ae5188f6a1039c7db3ad
Benjamin Fischer <c4spar@gmx.de>
2022-04-30 19:51:19 +0200

    refactor(flags,command): add equalsSign option

--- examples/command/action_handler.ts
+++ examples/command/action_handler.ts
@@ -5,7 +5,7 @@ import { Command } from "../../command/mod.ts";
 await new Command()
   .name("rm")
   .description("Remove directory.")
-  .option("-r, --recursive", "Remove directory recursively.")
+  .option("-r, --recursive=[val]", "Remove directory recursively.")
   .arguments("<dir>")
   .action(({ recursive }, dir: string) => {
     console.log("remove " + dir + (recursive ? " recursively" : ""));
