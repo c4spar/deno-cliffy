6abdc0aaf707104f116cbde9e783e27d845eea13
Benjamin Fischer <c4spar@gmx.de>
2022-05-10 22:46:09 +0200

    chore(upgrade): deno/std@0.138.0

--- dev_deps.ts
+++ dev_deps.ts
@@ -5,18 +5,18 @@ export {
   assertRejects,
   assertStrictEquals,
   assertThrows,
-} from "https://deno.land/std@0.137.0/testing/asserts.ts";
-export { assertSnapshot } from "https://deno.land/std@0.137.0/testing/snapshot.ts";
-export { describe, it } from "https://deno.land/std@0.137.0/testing/bdd.ts";
+} from "https://deno.land/std@0.138.0/testing/asserts.ts";
+export { assertSnapshot } from "https://deno.land/std@0.138.0/testing/snapshot.ts";
+export { describe, it } from "https://deno.land/std@0.138.0/testing/bdd.ts";
 export {
   bold,
   red,
   stripColor,
-} from "https://deno.land/std@0.137.0/fmt/colors.ts";
-export { dirname } from "https://deno.land/std@0.137.0/path/mod.ts";
-export { expandGlob } from "https://deno.land/std@0.137.0/fs/mod.ts";
-export type { WalkEntry } from "https://deno.land/std@0.137.0/fs/mod.ts";
-export { copy } from "https://deno.land/std@0.137.0/streams/conversion.ts";
+} from "https://deno.land/std@0.138.0/fmt/colors.ts";
+export { dirname } from "https://deno.land/std@0.138.0/path/mod.ts";
+export { expandGlob } from "https://deno.land/std@0.138.0/fs/mod.ts";
+export type { WalkEntry } from "https://deno.land/std@0.138.0/fs/mod.ts";
+export { copy } from "https://deno.land/std@0.138.0/streams/conversion.ts";
 
 /* 3rd party */
 export { gt, lt } from "https://deno.land/x/semver@v1.4.0/mod.ts";
