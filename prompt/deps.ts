e7dfaa516ded4f4495b4541c7f70c6724143e85d
Tristan F <LeoDog896@hotmail.com>
2022-08-26 10:19:46 -0400

    Re-update dependencies

--- prompt/deps.ts
+++ prompt/deps.ts
@@ -8,9 +8,9 @@ export {
   stripColor,
   underline,
   yellow,
-} from "https://deno.land/std@0.152.0/fmt/colors.ts";
+} from "https://deno.land/std@0.153.0/fmt/colors.ts";
 export {
   dirname,
   join,
   normalize,
-} from "https://deno.land/std@0.152.0/path/mod.ts";
+} from "https://deno.land/std@0.153.0/path/mod.ts";
