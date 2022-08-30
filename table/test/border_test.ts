60a781ceb1914cabf9f9d3b51124c6d1aab98108
Benjamin Fischer <61995275+c4spar@users.noreply.github.com>
2022-08-30 19:11:00 +0200

    fix(table): row doesn't inherit the table settings correctly (#428)

--- table/test/border_test.ts
+++ table/test/border_test.ts
@@ -6,7 +6,7 @@ Deno.test("default table chars", () => {
   assertEquals(
     Table
       .from([
-        ["Row 1 Column 1", "Row 1 Column 2", "Row 1 Column 3"],
+        ["Row 1 Column 1", "Row 1 Column 2"],
         ["Row 2 Column 1", "Row 2 Column 2", "Row 2 Column 3"],
         ["Row 3 Column 1", "Row 3 Column 2", "Row 3 Column 3"],
       ])
@@ -14,7 +14,7 @@ Deno.test("default table chars", () => {
       .toString(),
     `
 ┌────────────────┬────────────────┬────────────────┐
-│ Row 1 Column 1 │ Row 1 Column 2 │ Row 1 Column 3 │
+│ Row 1 Column 1 │ Row 1 Column 2 │                │
 ├────────────────┼────────────────┼────────────────┤
 │ Row 2 Column 1 │ Row 2 Column 2 │ Row 2 Column 3 │
 ├────────────────┼────────────────┼────────────────┤
