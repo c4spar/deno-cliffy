a9f69a1183c281dbf102683241fc84fb7cbcc413
Benjamin Fischer <c4spar@gmx.de>
2022-10-04 22:53:20 +0200

    print all example errors

--- _scripts/check_examples.ts
+++ _scripts/check_examples.ts
@@ -2,7 +2,14 @@ import { expandGlobSync } from "https://deno.land/std@0.158.0/fs/expand_glob.ts"
 
 const files = [...expandGlobSync("./examples/**/**.ts")];
 
-await Promise.all(files.map((file) => checkExample(file.path)));
+const result = await Promise.all(files.map((file) => checkExample(file.path)));
+const errors = result.filter((result) => result instanceof Error);
+
+if (errors.length) {
+  errors.forEach((error) => console.error(error));
+  console.error("%s examples have errors.", errors.length);
+  Deno.exit(1);
+}
 
 async function checkExample(file: string) {
   console.log("Type check example:", file);
@@ -10,7 +17,7 @@ async function checkExample(file: string) {
     args: ["check", "--unstable", file],
   });
   if (!output.success) {
-    throw new Error(
+    return new Error(
       `Type checking failed for ${file} \n${
         new TextDecoder().decode(output.stderr)
       }`,
