b394d33a91a019165f26f29e2f31404c02e0821e
Benjamin Fischer <c4spar@gmx.de>
2022-10-22 15:16:08 +0200

    don't export validateFlags

--- flags/mod.ts
+++ flags/mod.ts
@@ -1,6 +1,5 @@
 export * from "./flags.ts";
 export * from "./types.ts";
-export * from "./validate_flags.ts";
 export * from "./types/boolean.ts";
 export * from "./types/number.ts";
 export * from "./types/string.ts";
