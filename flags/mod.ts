3c11d391a3030ea4f68fb40b6f66905348b93ea3
Benjamin Fischer <c4spar@gmx.de>
2022-10-22 15:37:00 +0200

    export integer type

--- flags/mod.ts
+++ flags/mod.ts
@@ -1,6 +1,7 @@
 export * from "./flags.ts";
 export * from "./types.ts";
 export * from "./types/boolean.ts";
+export * from "./types/integer.ts";
 export * from "./types/number.ts";
 export * from "./types/string.ts";
 export * from "./deprecated.ts";
