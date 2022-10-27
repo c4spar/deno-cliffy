8a96562955491146b30392d32d4fa872adf6b747
Benjamin Fischer <c4spar@gmx.de>
2022-10-28 00:27:39 +0200

    docs(examples): don't use deprecated `OptionType`

--- examples/flags/flags.ts
+++ examples/flags/flags.ts
@@ -1,13 +1,12 @@
 #!/usr/bin/env -S deno run
 
-import { OptionType } from "../../flags/deprecated.ts";
 import { parseFlags } from "../../flags/flags.ts";
 
 console.log(parseFlags(Deno.args, {
   flags: [{
     name: "file",
     aliases: ["foo"],
-    type: OptionType.INTEGER,
+    type: "integer",
     // file cannot be combined with stdin option
     conflicts: ["stdin"],
   }],
