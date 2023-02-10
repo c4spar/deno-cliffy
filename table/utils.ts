a20b5dbb5399a0dcb2f201b253811d1cb6aa8d36
Benjamin Fischer <benjamin.fischer@adition.com>
2023-02-10 13:27:47 +0100

    fmt

--- table/utils.ts
+++ table/utils.ts
@@ -62,10 +62,10 @@ export const strLength = (str: string): number => {
   let length = 0;
   for (let i = 0; i < str.length; i++) {
     const charCode = str.charCodeAt(i);
-    // japanese characters \u3040 - \u30ff
-    // Check for chinese characters: \u4e00 - \u9fa5
     if (
+      // chinese characters: \u4e00 - \u9fa5
       (charCode >= 19968 && charCode <= 40869) ||
+      // japanese characters \u3040 - \u30ff
       (charCode >= 12352 && charCode <= 12543)
     ) {
       length += 2;
