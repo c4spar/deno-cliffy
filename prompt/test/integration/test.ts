95b941a7a4ab56efab0eb285debca2e706977541
Benjamin Fischer <c4spar@gmx.de>
2023-02-12 19:42:58 +0100

    add comment

--- prompt/test/integration/test.ts
+++ prompt/test/integration/test.ts
@@ -82,6 +82,7 @@ async function runPrompt(
 
   for (const input of inputs) {
     await writer.write(new TextEncoder().encode(input));
+    // Ensure all inputs are processed and rendered separately.
     await new Promise((resolve) => setTimeout(resolve, 200));
   }
 
