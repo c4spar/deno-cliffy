986168f72d90b5599cf1b935d4913fd0a684da55
Benjamin Fischer <61995275+c4spar@users.noreply.github.com>
2023-02-12 21:55:49 +0100

    test(prompt): improve integration tests (#544)

--- prompt/test/integration/fixtures/input_no_location_flag.ts
+++ prompt/test/integration/fixtures/input_no_location_flag.ts
@@ -1,6 +1,12 @@
 import { Input } from "../../../input.ts";
 
-await Input.prompt({
-  message: "Works without --location?",
-  default: "hope so",
-});
+export const tests = import.meta.main ? null : {
+  "should work without --location flag": ["yes", "\n"],
+};
+
+if (import.meta.main) {
+  await Input.prompt({
+    message: "Works without --location?",
+    default: "hope so",
+  });
+}
