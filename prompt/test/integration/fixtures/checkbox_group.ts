a7f5ba47dfe7351c785546fea140c6576e56e026
Benjamin Fischer <c4spar@gmx.de>
2023-02-12 22:04:22 +0100

    update snapshots

--- prompt/test/integration/fixtures/checkbox_group.ts
+++ prompt/test/integration/fixtures/checkbox_group.ts
@@ -1,16 +1,36 @@
+import { ansi } from "../../../../ansi/ansi.ts";
 import { Checkbox } from "../../../checkbox.ts";
 
-await Checkbox.prompt({
-  message: "Select an option",
-  options: [
-    { name: "Foo", value: "foo" },
-    { name: "Bar", value: "bar" },
-    {
-      name: "Baz",
-      options: [
-        { name: "Beep", value: "beep" },
-        { name: "Boop", value: "boop" },
-      ],
-    },
-  ],
-});
+export const tests = import.meta.main ? null : {
+  "should select a group option": ansi
+    .cursorDown
+    .cursorDown
+    .text(" ")
+    .text("\n")
+    .toArray(),
+  "should select a child option": ansi
+    .cursorDown
+    .cursorDown
+    .cursorForward
+    .cursorDown
+    .text(" ")
+    .text("\n")
+    .toArray(),
+};
+
+if (import.meta.main) {
+  await Checkbox.prompt({
+    message: "Select an option",
+    options: [
+      { name: "Foo", value: "foo" },
+      { name: "Bar", value: "bar" },
+      {
+        name: "Baz",
+        options: [
+          { name: "Beep", value: "beep" },
+          { name: "Boop", value: "boop" },
+        ],
+      },
+    ],
+  });
+}
--- prompt/test/integration/fixtures/select_group.ts
+++ prompt/test/integration/fixtures/select_group.ts
@@ -1,16 +1,28 @@
+import { ansi } from "../../../../ansi/ansi.ts";
 import { Select } from "../../../select.ts";
 
-await Select.prompt({
-  message: "Select an option",
-  options: [
-    { name: "Foo", value: "foo" },
-    { name: "Bar", value: "bar" },
-    {
-      name: "Baz",
-      options: [
-        { name: "Beep", value: "beep" },
-        { name: "Boop", value: "boop" },
-      ],
-    },
-  ],
-});
+export const tests = import.meta.main ? null : {
+  "should select a child option": ansi
+    .cursorDown
+    .cursorDown
+    .text("\n")
+    .text("\n")
+    .toArray(),
+};
+
+if (import.meta.main) {
+  await Select.prompt({
+    message: "Select an option",
+    options: [
+      { name: "Foo", value: "foo" },
+      { name: "Bar", value: "bar" },
+      {
+        name: "Baz",
+        options: [
+          { name: "Beep", value: "beep" },
+          { name: "Boop", value: "boop" },
+        ],
+      },
+    ],
+  });
+}
