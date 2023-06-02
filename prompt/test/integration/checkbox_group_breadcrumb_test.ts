f6d24dbaa36332ea4e5563a47c3f2bbfa1717f0d
Benjamin Fischer <c4spar@gmx.de>
2023-06-02 22:23:25 +0200

    adapt tests

---
+++ prompt/test/integration/checkbox_group_breadcrumb_test.ts
@@ -0,0 +1,53 @@
+import { ansi } from "../../../ansi/ansi.ts";
+import { snapshotTest } from "../../../testing/snapshot.ts";
+import { Checkbox } from "../../checkbox.ts";
+
+await snapshotTest({
+  name: "checkbox prompt with groups and breadcrumb",
+  meta: import.meta,
+  osSuffix: ["windows"],
+  steps: {
+    "should render breadcrumb": {
+      stdin: ansi
+        .cursorForward
+        .cursorForward
+        .cursorForward
+        .cursorForward
+        .cursorForward
+        .cursorForward
+        .cursorForward
+        .text(" ")
+        .text("\n")
+        .toArray(),
+    }
+  },
+  async fn() {
+    await Checkbox.prompt({
+      message: "Select an option",
+      breadcrumbSeparator: "#",
+      maxBreadcrumbItems: 4,
+      options: [{
+        name: "Foo",
+        options: [{
+          name: "Bar",
+          options: [{
+            name: "Baz",
+            options: [{
+              name: "Beep",
+              options: [{
+                name: "Boop",
+                options: [{
+                  name: "Fiz",
+                  options: [{
+                    name: "Faz",
+                    value: "123",
+                  }],
+                }],
+              }],
+            }],
+          }],
+        }],
+      }],
+    });
+  },
+});
