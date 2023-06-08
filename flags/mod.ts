d76b742815a37e8f35b31173436de7e196c15bd4
Benjamin Fischer <c4spar@gmx.de>
2023-06-08 12:48:08 +0200

    refactor(flags): use explicit exports in mod.ts

--- flags/mod.ts
+++ flags/mod.ts
@@ -1,8 +1,29 @@
-export * from "./flags.ts";
-export * from "./types.ts";
-export * from "./types/boolean.ts";
-export * from "./types/integer.ts";
-export * from "./types/number.ts";
-export * from "./types/string.ts";
-export * from "./deprecated.ts";
+export { parseFlags } from "./flags.ts";
+export {
+  type ArgumentOptions,
+  type ArgumentType,
+  type ArgumentValue,
+  type DefaultValue,
+  type DefaultValueHandler,
+  type FlagOptions,
+  type ParseFlagsContext,
+  type ParseFlagsOptions,
+  type TypeHandler,
+  type ValueHandler,
+} from "./types.ts";
+export { boolean } from "./types/boolean.ts";
+export { integer } from "./types/integer.ts";
+export { number } from "./types/number.ts";
+export { string } from "./types/string.ts";
+export {
+  type IDefaultValue,
+  type IFlagArgument,
+  type IFlagOptions,
+  type IFlagsResult,
+  type IFlagValueHandler,
+  type IParseOptions,
+  type ITypeHandler,
+  type ITypeInfo,
+  type OptionType,
+} from "./deprecated.ts";
 export { ValidationError } from "./_errors.ts";
