8dfad5cd1bb641d6a119470c7a07b16f2c03c73a
Benjamin Fischer <c4spar@gmx.de>
2023-07-30 11:32:08 +0200

    chore(upgrade): deno/std@0.196.0

--- command/deps.ts
+++ command/deps.ts
@@ -10,4 +10,4 @@ export {
   red,
   setColorEnabled,
   yellow,
-} from "https://deno.land/std@0.192.0/fmt/colors.ts";
+} from "https://deno.land/std@0.196.0/fmt/colors.ts";
--- prompt/deps.ts
+++ prompt/deps.ts
@@ -8,9 +8,9 @@ export {
   stripColor,
   underline,
   yellow,
-} from "https://deno.land/std@0.192.0/fmt/colors.ts";
+} from "https://deno.land/std@0.196.0/fmt/colors.ts";
 export {
   dirname,
   join,
   normalize,
-} from "https://deno.land/std@0.192.0/path/mod.ts";
+} from "https://deno.land/std@0.196.0/path/mod.ts";
