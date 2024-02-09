726a87b3a98edd6787e21a80c86c4a89630883b8
Asher Gomez <ashersaupingomez@gmail.com>
2024-02-09 11:35:38 +1100

    fmt

--- ansi/deps.ts
+++ ansi/deps.ts
@@ -1,6 +1,4 @@
-export {
-  encodeBase64,
-} from "https://deno.land/std@0.215.0/encoding/base64.ts";
+export { encodeBase64 } from "https://deno.land/std@0.215.0/encoding/base64.ts";
 export type {
   ReaderSync,
   WriterSync,
