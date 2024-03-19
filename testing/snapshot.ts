0c34bbdc3208b6cd66494ff3db453cb6202efbe6
Julien Oculi <xdrz.jot.sr@gmail.com>
2024-03-19 17:46:17 +0100

    refactor: :recycle: don't force `deno.jsonc` prefers default options

--- testing/snapshot.ts
+++ testing/snapshot.ts
@@ -174,8 +174,7 @@ async function runPrompt(
       stderr: "piped",
       args: [
         "run",
-        "-c=deno.jsonc",
-        ...options.denoArgs ?? ["--allow-env=SNAPSHOT_TEST_NAME"],
+        ...options.denoArgs ?? ["--allow-env=SNAPSHOT_TEST_NAME", "-c=deno.jsonc"],
         options.meta.url,
         ...options.args ?? [],
         ...step?.args ?? [],
