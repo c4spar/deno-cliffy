6d08b9a616476512bc3df359fe9866e6ed7cfa93
Benjamin Fischer <61995275+c4spar@users.noreply.github.com>
2024-05-15 00:25:53 +0200

    refactor(command): add exports for completions and upgrade provider (#700)

--- command/upgrade/mod.ts
+++ command/upgrade/mod.ts
@@ -1,17 +1,3 @@
-export {
-  DenoLandProvider,
-  type DenoLandProviderOptions,
-} from "./provider/deno_land.ts";
-export {
-  GithubProvider,
-  type GithubProviderOptions,
-  type GithubVersions,
-} from "./provider/github.ts";
-export { JsrProvider, type JsrProviderOptions } from "./provider/jsr.ts";
-export {
-  NestLandProvider,
-  type NestLandProviderOptions,
-} from "./provider/nest_land.ts";
 export { Provider, type UpgradeOptions, type Versions } from "./provider.ts";
 export {
   UpgradeCommand,
