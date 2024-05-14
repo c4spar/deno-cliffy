3c6d1759783791338c63a910f77c0e8d90048001
Benjamin Fischer <c4spar@gmx.de>
2024-05-15 00:32:12 +0200

    feat(command): add npm upgrade provider

---
+++ command/upgrade/provider/npm.ts
@@ -0,0 +1,66 @@
+import { Provider, type Versions } from "../provider.ts";
+
+export type NpmProviderOptions = {
+  package: string;
+} | {
+  scope: string;
+  name?: string;
+};
+
+export class NpmProvider extends Provider {
+  name = "npm";
+  private readonly repositoryUrl = "https://registry.npmjs.org/";
+  private readonly packageName?: string;
+  private readonly packageScope: string;
+
+  constructor(options: NpmProviderOptions) {
+    super();
+    this.packageScope = "package" in options
+      ? options.package.split("/")[0].slice(1)
+      : options.scope;
+    this.packageName = "package" in options
+      ? options.package.split("/")[1]
+      : options.name;
+  }
+
+  async getVersions(
+    name: string,
+  ): Promise<Versions> {
+    const response = await fetch(
+      `${this.repositoryUrl}/@${this.packageScope}/${this.packageName ?? name}`,
+    );
+    if (!response.ok) {
+      throw new Error(
+        "couldn't fetch the latest version - try again after sometime",
+      );
+    }
+
+    const { "dist-tags": { latest }, versions } = await response
+      .json() as NpmApiPackageMetadata;
+
+    return {
+      latest,
+      versions: Object.keys(versions).reverse(),
+    };
+  }
+
+  getRepositoryUrl(name: string): string {
+    return new URL(
+      `@${this.packageScope}/${this.packageName ?? name}`,
+      this.repositoryUrl,
+    ).href;
+  }
+
+  getRegistryUrl(name: string, version: string): string {
+    return `npm:@${this.packageScope}/${this.packageName ?? name}@${version}`;
+  }
+}
+
+type NpmApiPackageMetadata = {
+  "dist-tags": {
+    latest: string;
+  };
+  versions: {
+    [version: string]: unknown;
+  };
+};
