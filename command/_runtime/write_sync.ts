7f8675137eb38f02a5d3aac068ccaaddb371d1c3
Benjamin Fischer <c4spar@gmx.de>
2024-06-22 22:24:44 +0200

    fix writeSync method

--- command/_runtime/write_sync.ts
+++ command/_runtime/write_sync.ts
@@ -1,13 +1,12 @@
-export function writeSync(data: Uint8Array): void {
-  if ("Deno" in globalThis) {
-    // deno-lint-ignore no-explicit-any
-    (globalThis as any).Deno.stdout.writeSync(data);
-  } else if ("process" in globalThis) {
-    // deno-lint-ignore no-explicit-any
-    (globalThis as any).process.stdout.write(
-      // deno-lint-ignore no-explicit-any
-      (globalThis as any).Buffer.from(data),
-    );
+export function writeSync(data: Uint8Array): number {
+  // deno-lint-ignore no-explicit-any
+  const { Deno, process } = globalThis as any;
+
+  if (Deno) {
+    return Deno.stdout.writeSync(data);
+  } else if (process) {
+    process.stdout.write(data);
+    return data.byteLength;
   } else {
     throw new Error("unsupported runtime");
   }
