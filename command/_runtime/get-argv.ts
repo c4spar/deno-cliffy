ced45fc57984ea479bba897cb18d1347b2f24f4a
Benjamin Fischer <61995275+c4spar@users.noreply.github.com>
2024-09-28 22:44:33 +0200

    refactor(command): remove unused files (#751)

--- command/_runtime/get-argv.ts
+++
@@ -1,6 +0,0 @@
-export function getArgv(): Array<string> {
-  // deno-lint-ignore no-explicit-any
-  const { Deno, process } = globalThis as any;
-
-  return Deno?.args ?? process?.argv.slice(2) ?? [];
-}
