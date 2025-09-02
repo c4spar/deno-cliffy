09b7e914b53f6a2188382b68ddd3ebc4d3062d97
Benjamin Fischer <c4spar@gmx.de>
2025-09-02 19:15:11 +0200

    fix(command): fix support for options depending on environment variables

--- command/test/option/depends_test.ts
+++ command/test/option/depends_test.ts
@@ -57,3 +57,31 @@ test("command depends option with default value: should not accept --flag2 test"
     `Option "--flag2" depends on option "--flag1".`,
   );
 });
+
+test({
+  name: "command option should depend on environment variable",
+  fn: async () => {
+    const cmd = new Command()
+      .throwErrors()
+      .option("-f, --foo <value:string>", "foo")
+      .option("-b, --bar", "bar", {
+        depends: ["foo"],
+      })
+      .env("FOO=<value:string>", "foo");
+
+    Deno.env.set("FOO", "123");
+
+    const { options, args } = await cmd.parse(["--bar"]);
+
+    assertEquals(options, { foo: "123", bar: true });
+    assertEquals(args, []);
+
+    Deno.env.delete("FOO");
+
+    await assertRejects(
+      () => cmd.parse(["--bar"]),
+      Error,
+      `Option "--bar" depends on option "--foo".`
+    )
+  },
+});
