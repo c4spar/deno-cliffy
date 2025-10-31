9198261485e8a9960efeedacee80c4ed365f35ca
Benjamin Fischer <c4spar@gmx.de>
2025-11-01 00:09:39 +0100

    add jsdoc and move name into options object

--- command/completions/generate.ts
+++ command/completions/generate.ts
@@ -3,21 +3,51 @@ import { BashCompletionsGenerator } from "./_bash_completions_generator.ts";
 import { FishCompletionsGenerator } from "./_fish_completions_generator.ts";
 import { ZshCompletionsGenerator } from "./_zsh_completions_generator.ts";
 
+export interface GenerateShellCompletionsOptions {
+  name?: string;
+}
+
 /**
- * Generates shell completions script
+ * Generates shell completions for the specified shell.
+ *
+ * @param shell The target shell type ("bash", "fish", or "zsh").
+ * @param name The name of the command-line application.
+ * @param cmd The root Command instance representing the CLI structure.
+ * @returns A string containing the shell completion script.
+ * @throws {Error} If an unsupported shell type is specified.
+ *
+ * @example
+ * ```ts
+ * import { Command } from "@cliffy/command";
+ * import { generateShellCompletions } from "@cliffy/command/completions";
+ *
+ * const myCmd = new Command()
+ *   .name("myapp")
+ *   .description("My Application")
+ *   // ... define commands and options ...
+ *   ;
+ *
+ * const bashCompletions = generateShellCompletions(myCmd, "bash");
+ * console.log(bashCompletions);
+ * // This will output the bash completion script for "myapp".
+ *
+ * const zshCompletions = generateShellCompletions(myCmd, "zsh", { name: "myapp2" });
+ * console.log(zshCompletions);
+ * // This will output the zsh completion script and use "myapp2" as the command name.
+ * ```
  */
-export function generateCompletions(
-  shell: "bash" | "fish" | "zsh",
-  name: string,
+export function generateShellCompletions(
   cmd: Command,
+  shell: "bash" | "fish" | "zsh",
+  { name = cmd.getName() }: GenerateShellCompletionsOptions = {},
 ): string {
   switch (shell) {
     case "bash":
       return BashCompletionsGenerator.generate(name, cmd);
-    case "zsh":
-      return ZshCompletionsGenerator.generate(name, cmd);
     case "fish":
       return FishCompletionsGenerator.generate(name, cmd);
+    case "zsh":
+      return ZshCompletionsGenerator.generate(name, cmd);
     default:
       throw new Error(`Unsupported shell: ${shell}`);
   }
--- command/completions/mod.ts
+++ command/completions/mod.ts
@@ -2,4 +2,7 @@ export { BashCompletionsCommand } from "./bash.ts";
 export { FishCompletionsCommand } from "./fish.ts";
 export { ZshCompletionsCommand } from "./zsh.ts";
 export { CompletionsCommand } from "./completions_command.ts";
-export { generateCompletions } from "./generate.ts";
+export {
+  generateShellCompletions,
+  type GenerateShellCompletionsOptions,
+} from "./generate.ts";
