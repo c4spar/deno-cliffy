a729b808472429b96c315a7bef9eb2c5d1c08ade
Benjamin Fischer <c4spar@gmx.de>
2026-01-25 17:39:29 +0100

    fmt

--- table/mod.ts
+++ table/mod.ts
@@ -1,19 +1,6 @@
 export { type Border, border } from "./border.ts";
-export {
-  Cell,
-  type CellType,
-  type CellValue,
-  type Direction,
-} from "./cell.ts";
+export { Cell, type CellType, type CellValue, type Direction } from "./cell.ts";
 export { Column, type ColumnOptions } from "./column.ts";
 export { consumeWords } from "./consume_words.ts";
-export {
-  type DataRow,
-  Row,
-  type RowType,
-} from "./row.ts";
-export {
-  type BorderOptions,
-  Table,
-  type TableType,
-} from "./table.ts";
+export { type DataRow, Row, type RowType } from "./row.ts";
+export { type BorderOptions, Table, type TableType } from "./table.ts";
